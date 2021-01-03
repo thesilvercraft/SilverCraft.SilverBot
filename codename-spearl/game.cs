3460138bb20bc07a1fc0d0e45e3e7361f7cb5437
Filip Velickovski <46320280+Silverdimond@users.noreply.github.com>
2021-01-03 15:46:58 +0100

    e

---
+++ codename-spearl/game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codename_spearl
+{
+    internal class game
+    {
+        private async Task<ushort> addUserAsync(User user)
+        {
+            if (users.Contains(user))
+            {
+                return status.useralreadyjoined;
+            }
+            try
+            {
+                users.Add(user);
+                return status.success;
+            }
+            catch
+            {
+                //i fucked up something ahhhhhhhh
+                throw;
+            }
+        }
+
+        private ConcurrentBag<User> users { get; set; }
+    }
+
+    internal class status
+    {
+        public static ushort faliure = 0;
+        public static ushort success = 1;
+        public static ushort useralreadyjoined = 2;
+        public static ushort usernotin = 3;
+    }
+}
---
+++ codename-spearl/round.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codename_spearl
+{
+    internal class round
+    {
+        private User explainer { get; set; }
+        private User[] users { get; set; }
+        private User[] usersthatwon { get; set; }
+        private timespan timespan { get; set; }
+    }
+}

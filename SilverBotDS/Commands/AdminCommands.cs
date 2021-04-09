da4e99bb7363cfa3884fcf7b8063389ddfec915c
SilverDimond <46320280+Silverdimond@users.noreply.github.com>
2021-04-09 12:29:09 +0200

    fix the admin only commands

--- SilverBotDS/Commands/AdminCommands.cs
+++ SilverBotDS/Commands/AdminCommands.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace SilverBotDS.Commands
 {
-    [RequirePermissions(Permissions.Administrator)]
+    [RequireUserPermissions(Permissions.Administrator)]
     public class AdminCommands : BaseCommandModule
     {
         private DiscordEmoji[] _pollEmojiCache;

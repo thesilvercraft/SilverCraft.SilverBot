6da3c27744dcc842a6f81d8c5e7805fe863d15cb
SilverDimond <46320280+Silverdimond@users.noreply.github.com>
2021-05-23 18:26:41 +0200

    Fix requiredj attribute

--- SilverBotDS/Converters/RequireDJ.cs
+++ SilverBotDS/Converters/RequireDJ.cs
@@ -7,6 +7,8 @@ namespace SilverBotDS.Converters
 {
     public class RequireDJAttribute : CheckBaseAttribute
     {
-        public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help) => Task.FromResult(!ctx.Channel.IsPrivate && (ctx.Member.Roles.Any(e => e.CheckPermission(DSharpPlus.Permissions.ManageChannels) == DSharpPlus.PermissionLevel.Allowed || e.Name.ToLower().Contains("dj")) || (ctx.Member.VoiceState is not null && ctx.Member.VoiceState.Channel.Users.LongCount(x => !x.IsBot) == 1)));
+        public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help) => (help && ctx.Channel.IsPrivate) || ctx.Member.Roles.Any(e => e.CheckPermission(DSharpPlus.Permissions.ManageChannels) == DSharpPlus.PermissionLevel.Allowed || e.Name.ToLower().Contains("dj")) || (ctx.Member.VoiceState is not null && ctx.Member.VoiceState.Channel is not null && ctx.Member.VoiceState.Channel.Users is not null && ctx.Member.VoiceState.Channel.Users.LongCount(x => !x.IsBot) == 1)
+                ? Task.FromResult(true)
+                : Task.FromResult(false);
     }
 }

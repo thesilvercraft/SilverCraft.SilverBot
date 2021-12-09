8d6c455da4877c5636cd5758b3ab53ef4aeaabe7
Wbubbler <75879906+Wbubbler@users.noreply.github.com>
2021-12-09 20:21:46 +0100

    got the randomquotes

--- SilverBotDS.AnimeModule/Anime.cs
+++ SilverBotDS.AnimeModule/Anime.cs
@@ -2,6 +2,7 @@
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using SilverBotDS.Attributes;
+using SilverBotDS.Utils;
 using System.Net.Http;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -14,6 +15,7 @@ namespace SilverBotDS.Commands
     {
         public HttpClient Client { private get; set; }
         private const string BaseURL = "https://anime-api.hisoka17.repl.co/";
+        private string[] quotes = { "Playing a game by yourself is boring. You need a loser to crush so badly they can't recover.", "In our society, letting others find out that you're a nice person is a very risky move. It's extremely likely that someone would take advantage of that.", "Don't believe in the you that believes in me and don't believe in the me that believes in you. Believe in the you that believes in yourself!", "It is a poor knight indeed who slays one who defies him. The key is to show yourself to be superior that they will not defy you in the first place.", "There's something wrong with people who seek reality in fiction.", "There are things that cannot be taken back. But the world will keep on spinning whether you laugh or you cry. High school life will eventually end too.", "People don't realize they aren't able to choose whether or not to believe something. If they subconsciously believe something, therein lies the potential for a curse.", "Even if he hates me, being able to die with the knowledge that the one I love will never forget me… there is no greater happiness a woman could desire.", "There's something wrong with people who seek reality in fiction.", "People who don't work hard don't have the right to be envious of the people with talent. People fail because they don't understand the hard work necessary to be successful.", "You may be unfortunate, but that doesn't mean you have to suffer. You may not be blessed, but that doesn't mean you have to throw a fit over it. Even if bad things happen to you, just be strong!", "Those with talent who aren't aware of themselves cause pain for those who have none.", "If I'm getting in trouble no matter what I do, I may as well make a grand frontal assault and fight to my last breath.", "I have someone I like. I never had the confidence and I’ve doubted my feelings countless of times. But, when she stays by my side and smiles with me, I feel like I can do anything.", "Well, it's an anime, so you shouldn't think so hard about it.", "A faint clap of thunder, clouded skies; perhaps rain comes. If so, will you stay here with me?", "The fake is of far greater value. In its deliberate attempt to be real, it's more real than the real thing." };
 
         private async Task<string> GetAnimeUrl(string endpoint)
         {
@@ -26,6 +28,10 @@ namespace SilverBotDS.Commands
                                              .WithEmbed(new DiscordEmbedBuilder().WithImageUrl(url))
                                              .SendAsync(ctx.Channel);
         }
+
+        [Command("randomquote")]
+        [Description("Gives you a random anime quote")]
+        public async Task RandomQuote(CommandContext ctx) => await ctx.Message.RespondAsync(quotes[RandomGenerator.Next(0, quotes.Length)]);
 
         [Command("hug")]
         [Description("i have no idea what this means")]

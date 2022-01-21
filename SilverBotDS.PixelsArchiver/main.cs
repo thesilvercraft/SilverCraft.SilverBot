c472de817c97a0976be2562883d85647df884476
SilverDimond <46320280+Silverdimond@users.noreply.github.com>
2022-01-21 21:58:33 +0100

    Update main.cs

--- SilverBotDS.PixelsArchiver/main.cs
+++ SilverBotDS.PixelsArchiver/main.cs
@@ -38,14 +38,21 @@ namespace SilverBotDS.PixelsArchiver
         {
             Dictionary<string, Stream> strm = new();
             foreach (var urls in ((PixelsArchiverConfig)gaming).ApisToArchivePicturesFrom)
-            {
-                using var request = new HttpRequestMessage(HttpMethod.Get, urls.Key);
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", urls.Value);
-                var response = await Client.SendAsync(request);
-                if (response.IsSuccessStatusCode)
+            {   
+                try
                 {
-                    var stuffineed = JsonSerializer.Deserialize<Rootobject>(await response.Content.ReadAsStringAsync());
-                    strm.Add(new Uri(urls.Key).DnsSafeHost + RandomGenerator.RandomAbcString(6) + ".png", new MemoryStream(Convert.FromBase64String(stuffineed.DataURL[22..])));
+                    using var request = new HttpRequestMessage(HttpMethod.Get, urls.Key);
+                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", urls.Value);
+                    var response = await Client.SendAsync(request);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var stuffineed = JsonSerializer.Deserialize<Rootobject>(await response.Content.ReadAsStringAsync());
+                        strm.Add(new Uri(urls.Key).DnsSafeHost + RandomGenerator.RandomAbcString(6) + ".png", new MemoryStream(Convert.FromBase64String(stuffineed.DataURL[22..])));
+                    }
+                }
+                catch(Exception e)
+                {
+                    //the api's can get a bit quirky at night
                 }
             }
             var zipFile = new MemoryStream();

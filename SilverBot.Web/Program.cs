be244a391ada06db3f10e01eb1598d8e88906614
Silverdimond <46320280+Silverdimond@users.noreply.github.com>
2022-11-27 14:38:13 +0100

    GPL3 headers

--- SilverBot.Web/Program.cs
+++ SilverBot.Web/Program.cs
@@ -1,3 +1,8 @@
+/* This file is part of SilverBot.
+SilverBot is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+SilverBot is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with SilverBot. If not, see <https://www.gnu.org/licenses/>.
+*/
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Radzen;
 using SilverBotDS.Objects;
